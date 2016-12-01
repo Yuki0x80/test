53b10bbd414ca612ba1ce78591f74334772c843d
saiyuki1919 <nana.saiyuki1919@gmail.com>
2016-12-02 00:19:12 +0900

    new

--- DjangoWebProject/DjangoWebProject/app/views.py
+++ DjangoWebProject/DjangoWebProject/app/views.py
@@ -14,7 +14,7 @@ def home(request):
         request,
         'app/index.html',
         {
-            'title':'Home Page',
+            'title':'Home',
             'year':datetime.now().year,
         }
     )
